1d1fcaa453a5ee2155a1df3c2bed47fb327a523c
Oleg Plotnikov <plotnikov@multivi.ru>
2015-07-31 11:52:17 +0500

    * isPlainFile fix

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,9 @@
 function isPlainFile() {
-	var pre = document.body.children[0];
-	return pre.nodeName === 'PRE' && pre.attributes[0].value === 'word-wrap: break-word; white-space: pre-wrap;';
+	var pre = document.body ? document.body.children[0] : null;
+	if (pre) {
+		var style = pre.attributes[0] ? pre.attributes[0].value : null;
+		return pre.nodeName === 'PRE' && style === 'word-wrap: break-word; white-space: pre-wrap;';
+	} else {
+		return false;
+	}
 }
